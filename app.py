066ca75d7c39612e875b2290a7b46a358b4e086d
Salas-Carlos <45397749+Salas-Carlos@users.noreply.github.com>
2019-09-05 21:55:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import threading
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
+class observador(threading.Thread):
+   def run(self):
+      print ("Acaba de entrar a la Lista")
+
+class observador1(threading.Thread):
+   def run(self):
+      print ("Acaba de entrar al Json")
 
 class Singleton:
 	_instance = None
@@ -32,18 +40,20 @@ def inicio():
 
 @app.route('/users/list')
 def usersJ():
-  
+  tt = observador()
+  tt.start()
 
   print (hex(id(lista1)))
   return render_template('index.html',lista=lista1)
 
 @app.route('/api/v1/users')
 def usersJson():
-  
+  t = observador1()
+  t.start()
   datos=[]
   print (hex(id(lis)))
   for i in range(len(lis)):
     datos.append({'Firstname':lis[i][0],'Lastname':lis[i][1],'Username':lis[i][2],'Email':lis[i][3],'Password':lis[i][4]})
   return jsonify(datos)
  
-app.run(debug=True,  host='0.0.0.0', port=80)
+app.run(debug=True, host="0.0.0.0")
